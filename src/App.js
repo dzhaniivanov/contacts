5028b2c5a8014231e3dcb6e2ce520dea2af6f09a
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-06-25 16:41:29 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { uuid } from 'uuidv4';
 import './App.css';
 import Header from "./components/Header";
@@ -33,9 +34,17 @@ function App() {
 
   return (
     <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" component={ContactList} exact />
+          <Route path="/add" component={AddContact} />
+        </Switch>
+
+        {/*  {<AddContact addContactHandler={addContactHandler} />}
+        <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+      </Router>
+
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,23 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
 
-    const deleteContactHandler =(id)=>{
+    const deleteContactHandler = (id) => {
         props.getContactId(id);
     };
 
-    const renderContactList = props.contacts.map((contact) => {
+
+    const contacts = [
+        {
+            id: "1",
+            name: "bla",
+            email: 'bla@abv.bg',
+        },
+        {
+            id: "1",
+            name: "bla",
+            email: 'bla@abv.bg',
+        },
+        {
+            id: "1",
+            name: "bla",
+            email: 'bla@abv.bg',
+        },
+    ]
+    const renderContactList = contacts.map((contact) => {
         return (
-            <ContactCard contact={contact}  clickHandler={deleteContactHandler} key={contact.id}/>
+            <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id} />
         )
     })
 
 
 
     return (
-        <div className="ui celled list">
-            {renderContactList}
+        <div className="main">
+            <h2>
+                Contact List
+                <button className="ui button blue right">Add Contact</button>
+            </h2>
+            <div className="ui ceiled list">
+                {renderContactList}
+            </div>
+
         </div>
     )
 
